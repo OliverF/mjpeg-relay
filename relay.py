baa6bf136c2357749db12b8b552babe32cfe1a48
Oliver Foyle <ofoyle@gmail.com>
2015-02-10 13:57:21 +0000

    Add support for relaying via WebSockets

--- relay.py
+++ relay.py
@@ -8,48 +8,92 @@ import Queue
 import re
 import logging
 import requests
+import base64
+try:
+	from SimpleWebSocketServer.SimpleWebSocketServer import WebSocket, SimpleWebSocketServer
+except ImportError, e:
+	print "Failed to import dependency: {}".format(e)
+	print "Please ensure the SimpleWebSocketServer submodule has been correctly installed: git submodule update --init"
+	sys.exit(1)
 
-class StreamingClient:
+class StreamingClient(object):
 
-	def __init__(self, sock):
-		self.sock = sock
-		sock.settimeout(5) #long timeout to allow clients some flexibility
+	def __init__(self):
 		self.streamBuffer = ""
 		self.streamQueue = Queue.Queue()
 		self.streamThread = threading.Thread(target = self.stream)
 		self.streamThread.daemon = True
 		self.connected = True
 		self.kill = False
+		super(StreamingClient, self).__init__()
 
 	def start(self):
 		self.streamThread.start()
+
+	def transmit(self, data):
+		return len(data)
+
+	def stop(self):
+		pass
 
 	def bufferStreamData(self, data):
 		#use a thread-safe queue to ensure stream buffer is not modified while we're sending it
 		self.streamQueue.put(data)
 
 	def stream(self):
-		while True:
+		while self.connected:
 			if (self.kill):
-				self.sock.close()
+				self.stop()
 				return
 
 			if (not self.streamQueue.empty()):
 				self.streamBuffer += self.streamQueue.get()
 
 			if (len(self.streamBuffer) > 0):
-				try:
-					streamedTo = self.sock.send(self.streamBuffer)
+				streamedTo = self.transmit(self.streamBuffer)
+				if (streamedTo and streamedTo >= 0):
 					self.streamBuffer = self.streamBuffer[streamedTo:]
-				except socket.error, e:
-					self.connected = False
-					self.sock.close()
-					return
+				else:
+					self.streamBuffer = ""
 
-class RequestHandler:
-	"""Handles the initial connection with the client"""
+class TCPStreamingClient(StreamingClient):
+	def __init__(self, sock):
+		super(TCPStreamingClient, self).__init__()
+		self.sock = sock
+		self.sock.settimeout(5)
 
-	def __init__(self, port, broadcast, status):
+	def stop(self):
+		self.sock.close()
+
+	def transmit(self, data):
+		try:
+			return self.sock.send(data)
+		except socket.error, e:
+			self.connected = False
+			self.sock.close()
+
+class WebSocketStreamingClient(WebSocket, StreamingClient):
+	def __init__(self, *args, **kwargs):
+		super(WebSocketStreamingClient, self).__init__(*args, **kwargs)
+
+	def stop(self):
+		pass
+
+	def transmit(self, data):
+		self.sendMessage("data:image/jpg;base64," + data)
+		return len(data)
+
+	def handleConnected(self):
+		self.start()
+		Broadcaster._instance.webSocketClients.append(self)
+
+	def handleClose(self):
+		self.connected = False
+
+class HTTPRequestHandler:
+	"""Handles the initial connection with HTTP clients"""
+
+	def __init__(self, port):
 		#response we'll send to the client, pretending to be from the real stream source
 		dummyHeaderfh = open('dummy.header', 'r')
 		self.dummyHeader = dummyHeaderfh.read()
@@ -65,8 +109,8 @@ class RequestHandler:
 		self.acceptsock.bind(("0.0.0.0", port))
 		self.acceptsock.listen(10)
 
-		self.broadcast = broadcast
-		self.status = status
+		self.broadcast = Broadcaster._instance
+		self.status = Status._instance
 
 		self.kill = False
 
@@ -108,7 +152,7 @@ class RequestHandler:
 			#explicitly deal with individual requests. Verbose, but more secure
 			if ("/status" in requestPath):
 				clientsock.sendall('HTTP/1.0 200 OK\r\nContentType: text/html\r\n\r\n')
-				clientsock.sendall(self.statusHTML.format(clientcount = self.broadcast.clientCount, bwin = float(self.status.bandwidthIn*8)/1000000, bwout = float(self.status.bandwidthOut*8)/1000000))
+				clientsock.sendall(self.statusHTML.format(clientcount = self.broadcast.getClientCount(), bwin = float(self.status.bandwidthIn*8)/1000000, bwout = float(self.status.bandwidthOut*8)/1000000))
 				clientsock.close()
 			elif ("/style.css" in requestPath):
 				clientsock.sendall('HTTP/1.0 200 OK\r\nContentType: text/html\r\n\r\n')
@@ -118,7 +162,7 @@ class RequestHandler:
 				if (self.broadcast.broadcasting):
 					logging.info("Client connected, sending dummy header")
 					clientsock.sendall(self.dummyHeader.format(boundaryKey = self.broadcast.boundarySeparator))
-					client = StreamingClient(clientsock)
+					client = TCPStreamingClient(clientsock)
 					client.start()
 					logging.info("Adding client to join waiting queue")
 					self.broadcast.joiningClients.put(client) #blocking, no timeout
@@ -126,6 +170,8 @@ class RequestHandler:
 					clientsock.close()
 			elif ("/snapshot" in requestPath):
 				clientsock.sendall('HTTP/1.0 200 OK\r\n')
+				clientsock.sendall('Content-Type: image/jpeg\r\n')
+				clientsock.sendall('Content-Length: {}\r\n\r\n'.format(len(self.broadcast.lastFrame)))
 				clientsock.sendall(self.broadcast.lastFrame)
 				clientsock.close()
 			else:
@@ -149,15 +195,18 @@ class RequestHandler:
 class Broadcaster:
 	"""Handles relaying the source MJPEG stream to connected clients"""
 
-	def __init__(self, url, status):
+	_instance = None
+
+	def __init__(self, url):
 		self.headerType = "multipart/x-mixed-replace"
 		self.url = url
 
 		self.clients = []
+		self.webSocketClients = []
 		self.joiningClients = Queue.Queue()
 		self.clientCount = 0
 
-		self.status = status
+		self.status = Status._instance
 
 		self.kill = False
 		self.broadcastThread = threading.Thread(target = self.streamFromSource)
@@ -180,6 +229,8 @@ class Broadcaster:
 		except IOError, e:
 			logging.warning("Unable to read feedlost.jpeg: {}".format(e))
 			self.feedLostFrame = False
+
+		Broadcaster._instance = self
 
 	def start(self):
 		if (self.connectToStream()):
@@ -222,6 +273,29 @@ class Broadcaster:
 			return None
 
 	#
+	# Returns the total number of connected clients
+	#
+	def getClientCount(self):
+		return len(self.clients) + len(self.webSocketClients)
+
+	#
+	# Process data in frame buffer, extract frames when present
+	#
+	def extractFrames(self, frameBuffer):
+		if (frameBuffer.count(self.boundarySeparator) == 2):
+			#calculate the start and end points of the frame
+			start = frameBuffer.find(self.boundarySeparator) + (len(self.boundarySeparator) - 1)
+			end = frameBuffer.find(self.boundarySeparator, start)
+
+			#extract frame data
+			imageStart = frameBuffer.find("\r\n\r\n", start) + len("\r\n\r\n")
+			image = frameBuffer[imageStart:end]
+
+			return (image, end)
+		else:
+			return (None,0)
+
+	#
 	# Broadcast data to all connected clients
 	#
 	def broadcast(self, data):
@@ -229,22 +303,24 @@ class Broadcaster:
 		for client in self.clients:
 			if (not client.connected):
 				self.clients.remove(client)
-				self.clientCount -= 1
-				logging.info("Client left. Client count: {}".format(len(self.clients)))
+				logging.info("Client left. Client count: {}".format(self.getClientCount()))
 			client.bufferStreamData(data)
 
-		self.status.addToBytesIn(len(data))
-		self.status.addToBytesOut(len(data)*len(self.clients))
-
 		self.lastFrameBuffer += data
-		if (self.lastFrameBuffer.count(self.boundarySeparator) == 2):
-			#calculate the start and end points of the frame
-			start = self.lastFrameBuffer.find(self.boundarySeparator) + (len(self.boundarySeparator) - 1)
-			end = self.lastFrameBuffer.find(self.boundarySeparator, start)
-			#extract latest frame data
-			self.lastFrame = self.lastFrameBuffer[start:end]
+		frame, bufferProcessedTo = self.extractFrames(self.lastFrameBuffer)
+		if (frame):
 			#delete the frame now that it has been extracted, keep what remains in the buffer (faster, won't miss part of the next frame)
-			self.lastFrameBuffer = self.lastFrameBuffer[end:]
+			self.lastFrameBuffer = self.lastFrameBuffer[bufferProcessedTo:]
+
+			#save for /snapshot requests
+			self.lastFrame = frame
+
+			#serve to websocket clients
+			for client in self.webSocketClients:
+				if (not client.connected):
+					self.webSocketClients.remove(client)
+					logging.info("Client left. Client count: {}".format(self.getClientCount()))
+				client.bufferStreamData(base64.b64encode(frame))
 
 		if (not self.joiningClients.empty()):
 			pos = data.find(self.boundarySeparator)
@@ -260,7 +336,6 @@ class Broadcaster:
 						client = self.joiningClients.get()
 						client.bufferStreamData(catchup)
 						self.clients.append(client)
-						self.clientCount += 1
 						logging.info("Client has joined! Client count: {}".format(len(self.clients)))
 					except Exception, e:
 						logging.info("Failed to join client to stream: {}".format(e))
@@ -273,10 +348,12 @@ class Broadcaster:
 			try:
 				for data in self.sourceStream.iter_content(1024):
 					if (self.kill == True):
-						for client in self.clients:
+						for client in self.clients + self.webSocketClients:
 							client.kill = True
 						return
 					self.broadcast(data)
+					self.status.addToBytesIn(len(data))
+					self.status.addToBytesOut(len(data)*self.getClientCount())
 			except Exception, e:
 				logging.error("Lost connection to the stream source: {}".format(e))
 			finally:
@@ -290,14 +367,18 @@ class Broadcaster:
 					time.sleep(5)
 					self.connectToStream()
 
-
 class Status:
+
+	_instance = None
+
 	def __init__(self):
 		self.bytesOut = 0
 		self.bytesIn = 0
 
 		self.bandwidthOut = 0
 		self.bandwidthIn = 0
+
+		Status._instance = self
 
 	def addToBytesOut(self, byteCount):
 		self.bytesOut += byteCount
@@ -329,7 +410,8 @@ def quit():
 if __name__ == '__main__':
 	op = OptionParser(usage = "%prog [options] stream-source-url")
 
-	op.add_option("-p", "--port", action="store", default = 54321, dest="port", help = "Port to broadcast the MJPEG stream on")
+	op.add_option("-p", "--port", action="store", default = 54321, dest="port", help = "Port to serve the MJPEG stream on")
+	op.add_option("-w", "--ws-port", action="store", default = 54322, dest="wsport", help = "Port to serve the MJPEG stream on via WebSockets")
 	op.add_option("-q", "--quiet", action="store_true", default = False, dest="quiet", help = "Silence non-essential output")
 
 	(options, args) = op.parse_args()
@@ -353,11 +435,16 @@ if __name__ == '__main__':
 	statusthread.daemon = True
 	statusthread.start()
 
-	broadcast = Broadcaster(args[0], status)
+	broadcast = Broadcaster(args[0])
 	broadcast.start()
 
-	requestHandler = RequestHandler(options.port, broadcast, status)
+	requestHandler = HTTPRequestHandler(options.port)
 	requestHandler.start()
+
+	s = SimpleWebSocketServer('', options.wsport, WebSocketStreamingClient)
+	webSocketHandlerThread = threading.Thread(target=s.serveforever)
+	webSocketHandlerThread.daemon = True
+	webSocketHandlerThread.start()
 
 	try:
 		while raw_input() != "quit":
